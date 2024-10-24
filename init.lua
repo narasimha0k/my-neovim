1b5239925bf8ef9aaffd712b9c13b2a9767833d3
bablu0k <sreedevikalavakuri919@gmail.com>
2024-10-24 10:33:43 +0530

    Add plugins: alpha, autoformatting, autocompleting, lsp, comment

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- requirements
+ -- requirements
 require "core.options"
 require "core.keymaps"
 
@@ -21,6 +21,10 @@ require 'plugins.bufferline',
 require 'plugins.lualine',
 require 'plugins.treesitter',
 require 'plugins.telescope',
--- require 'plugins.lsp'
+require 'plugins.alpha',
+require 'plugins.comment',
+require 'plugins.autoformatting',
+require 'plugins.lsp',
+require 'plugins.autocompletion',
 -- SETUP END
 })
